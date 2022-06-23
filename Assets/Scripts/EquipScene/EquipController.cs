71ceefad7688d936d299196c3689f271e9c0b379
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2022-06-23 18:21:14 +0900

    .

--- Assets/Scripts/EquipScene/EquipController.cs
+++ Assets/Scripts/EquipScene/EquipController.cs
@@ -34,7 +34,6 @@ public class EquipController : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
