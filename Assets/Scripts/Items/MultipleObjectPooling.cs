e078599941642e812e8a6c2b6d6b2c7256dd1f97
Lee SeungEun <dltmddjs98@naver.com>
2022-07-04 21:44:40 +0900

    스킬이펙트 오브젝트풀링

--- Assets/Scripts/Items/MultipleObjectPooling.cs
+++ Assets/Scripts/Items/MultipleObjectPooling.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class MultipleObjectPooling : MonoBehaviour
 {
+    //public int SkillNum;
     public GameObject[] poolPrefabs;
     public int poolingCount;
 
-    private Dictionary<object, List<GameObject>> pooledObjects = new Dictionary<object, List<GameObject>>();
+    private Dictionary<string, List<GameObject>> pooledObjects = new Dictionary<string, List<GameObject>>();
 
     public void Awake()
     {
+        //poolPrefabs = new GameObject[SkillNum];
+        //poolingCount = new int[SkillNum];
+
         CreateMultiplePoolObjects();
     }
     public void CreateMultiplePoolObjects()
@@ -19,7 +23,7 @@ public class MultipleObjectPooling : MonoBehaviour
         {
             for (int j = 0; j < poolingCount; j++)
             {
-                if (!pooledObjects.ContainsKey(poolPrefabs[i].name))
+                if (!pooledObjects.ContainsKey(poolPrefabs[i].name))  // Dictionary에 해당 키가없으면 실행
                 {
                     List<GameObject> newList = new List<GameObject>();
                     pooledObjects.Add(poolPrefabs[i].name, newList);
@@ -31,22 +35,36 @@ public class MultipleObjectPooling : MonoBehaviour
             }
         }
     }
-
+    
+    public void AddObject(string _name)
+    {
+        for (int i = 0; i < poolPrefabs.Length; i++)
+        {
+            if(poolPrefabs[i].name == _name)  // 프리팹에서 동일한 것을 찾아서 생성
+            {
+                GameObject newDoll = Instantiate(poolPrefabs[i], transform);
+                pooledObjects[poolPrefabs[i].name].Add(newDoll);
+                return;
+            }
+        }
+    }
     public GameObject GetPooledObject(string _name)
     {
         if (pooledObjects.ContainsKey(_name))
         {
             for (int i = 0; i < pooledObjects[_name].Count; i++)
             {
-                if (!pooledObjects[_name][i].activeSelf)
+                if (!pooledObjects[_name][i].activeSelf)  // 비활성화된 오브젝트에 접근
                 {
+                    pooledObjects[_name][i].SetActive(true);  // 활성화 후 반환
                     return pooledObjects[_name][i];
                 }
             }
-
+            // 비활성화된 오브젝트가 없으면 (= 모두 활성화 상태이면)
             int beforeCreateCount = pooledObjects[_name].Count;
 
-            CreateMultiplePoolObjects();
+            //CreateMultiplePoolObjects();
+            AddObject(_name);
 
             return pooledObjects[_name][beforeCreateCount];
         }
