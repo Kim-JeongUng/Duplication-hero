aea5b809bc6c720b7a255f0f5963b18f0f5d08db
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2022-07-07 22:46:06 +0900

    flower monster

---
+++ Assets/Scripts/InGame/Enemy/Enemies/Flower.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Flower : StandEnemy
+{
+	[SerializeField] Shooter shooter;
+	[SerializeField] EnemyAimer aimer;
+
+	float lastShootTime;
+
+	public GameObject getSkill;  
+
+	protected new void Awake()
+	{
+		base.Awake();
+		
+		if (shooter == null)
+			shooter = GetComponentInChildren<Shooter>();
+		if (aimer == null)
+			aimer = GetComponentInChildren<EnemyAimer>();
+	}
+
+	protected void Update()
+	{
+		if (aimer.Target != null)
+		{
+			aimer.FollowTarget();
+			if (Time.time - lastShootTime >= (1 / attackSpeed))
+			{
+				lastShootTime = Time.time;
+				shooter.Shoot(new DamageReport(damage, this));
+			}
+		}
+	}
+	protected new void FixedUpdate()
+	{
+		base.FixedUpdate();
+		if (aimer.Target == null)
+			aimer.Aim();
+		else if (!aimer.IsVisible())
+			aimer.ResetTarget();
+		
+	}
+	protected override void Death(Entity killer)
+	{
+		Debug.Log("------------Monster Dead-------------");
+		shooter.Dispose();
+		// 확률에따라 스킬구슬, 장비아이템, 꽝 3가지 중에서 랜덤 실행
+
+		base.Death(killer);
+
+		if(OnDie == true)
+			DropItem(getSkill); // 스킬아이템 드랍
+	}
+}
---
+++ Assets/Scripts/InGame/Enemy/StandEnemy.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public abstract class StandEnemy : Enemy
+{
+    float movingStateTimer = 0;
+
+    protected new void Awake()
+    {
+        base.Awake();
+    }
+
+    protected void FixedUpdate()
+    {
+       /* switch (walkingState)
+        {
+            case MovingState.STAYING:
+                if(Time.time - movingStateTimer >= waitingTime)
+                {
+                    if (touchingPlayer != null)
+                    {
+                        if (touchingPlayer.TakeDamage(new DamageReport(damage * touchDamageMultiplier, this)))
+                            touchingPlayer = null;
+                        movingStateTimer = Time.time;
+                    }
+                    else
+                    {
+                        walkingState = MovingState.MOVING;
+                        movingStateTimer = Time.time + Random.Range(0, randomTime);
+                    }
+                }
+                else
+                {
+                }
+                break;
+            default:
+                break;
+        }*/
+    }
+
+    protected new void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        if (other.tag == Tags.playerTag)
+        {
+            walkingState = MovingState.STAYING;
+            movingStateTimer = Time.time;
+        }
+    }
+}
