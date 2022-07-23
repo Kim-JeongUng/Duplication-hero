4d837bbee6ef45c7a282c783f5c7dd809ca4a776
Yunjae Jang <jjw1270@naver.com>
2022-07-23 16:14:18 +0900

    playerBackHp

--- Assets/Scripts/InGame/UI/HealthBar.cs
+++ Assets/Scripts/InGame/UI/HealthBar.cs
@@ -7,18 +7,47 @@ public class HealthBar : MonoBehaviour
 	[SerializeField] Transform thisObject;
 	[SerializeField] Entity entity;
 	[SerializeField] Slider hpBar;
+	[SerializeField] Slider backHpBar;
 	[SerializeField] GameObject HpLineFolder;
 	[SerializeField] TMP_Text thisObjectHpText;
 	[SerializeField] float unitHp = 200f;
 	float defaultHp = 1000f;
+	float yPos;
+	float currentHp;
+	bool backHpHit;
 	private void Awake() {
-		GetHpBoost();
+		yPos = transform.position.y;
+		currentHp = entity.Hp;
+		if(entity.CompareTag("Player")){
+			GetHpBoost();
+		}
 	}
 	private void Update()
 	{
-		transform.position = thisObject.position;
+		if(entity.CompareTag("Player"))
+			transform.position = new Vector3(thisObject.position.x, yPos, thisObject.position.z);
+		else
+			transform.rotation = Quaternion.Euler(new Vector3(-90, -this.transform.root.rotation.y, 0));
+
         hpBar.value = Mathf.Lerp(hpBar.value, entity.Hp / entity.MaxHp, Time.deltaTime * 5f);
+
+		if(currentHp!=entity.Hp){
+			currentHp = entity.Hp;
+			Invoke("BackHp", 0.5f);
+		}
+
+		if(backHpHit){
+			backHpBar.value = Mathf.Lerp(hpBar.value, entity.Hp / entity.MaxHp, Time.deltaTime * 10f);
+			if(hpBar.value >= backHpBar.value - 0.01f){
+				backHpHit = false;
+				backHpBar.value = hpBar.value;
+			}
+		}
+
         thisObjectHpText.text = "" + entity.Hp;
+	}
+	void BackHp(){
+		backHpHit = true;
 	}
 
 	public void GetHpBoost()
